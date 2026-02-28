722b9e7256dad520a5ccbaab18d1e795b810f841
Chomiii19 <jomariborines55@gmail.com>
2026-03-01 00:00:55 +0800

    update

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { type IUser } from "../@types/interface";
 import { AuthContext } from "../hooks/useAuth";
+import { BACKEND_DOMAIN } from "../configs/config";
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<IUser | null>(null);
@@ -10,9 +11,12 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     const fetchMe = async () => {
       try {
-        const res = await axios.get("/api/v1/users/my-account", {
-          withCredentials: true, // IMPORTANT if using cookies
-        });
+        const res = await axios.get(
+          `${BACKEND_DOMAIN}/api/v1/users/my-account`,
+          {
+            withCredentials: true, // IMPORTANT if using cookies
+          },
+        );
 
         setCurrentUser(res.data.data);
       } catch {
